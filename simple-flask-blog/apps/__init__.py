2a94a75d1cce72a730abab720554d0fa883c83fa
mark <markwu0821@gamil.com>
2026-04-26 12:36:09 +0800

    add dockerfile

--- simple-flask-blog/apps/__init__.py
+++ simple-flask-blog/apps/__init__.py
@@ -6,16 +6,19 @@ from flask import Flask
 from flask_login import LoginManager
 
 from models.user import User
+from config.settings import db
 
 load_dotenv()
 
 ROOT_PATH = Path().absolute()
-TEMPLAGE_FOLDER  = ROOT_PATH / "templates"
+# ROOT_PATH = Path(__file__).resolve().parents[2]
+TEMPLATE_FOLDER  = ROOT_PATH / "templates"
 DB_PATH = ROOT_PATH / "db" / "blog.db"
 
-app = Flask(__name__, template_folder = TEMPLAGE_FOLDER )
+app = Flask(__name__, template_folder = TEMPLATE_FOLDER )
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
 app.secret_key = os.getenv("APP_SECRET_KEY")
+# app.secret_key = "dev-secret-key"
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -24,4 +27,5 @@ login_manager.login_message = "請登入會員帳號"
 
 @login_manager.user_loader
 def load_user(id) -> User:
-    return User.query.get(id)
+    return db.session.get(User, int(id))
+    # return User.query.get(int(id))
